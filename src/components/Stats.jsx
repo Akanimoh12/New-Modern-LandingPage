930900e369675a1fa76342aff5efc1391f7ff122
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-04-04 22:01:22 +0100

    Akanimoh: Updated the Stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,9 +2,19 @@ import { stats } from "../constants"
 import styles from "../style"
 
 const Stats = () => (
-    <section className={`${styles.flexCenter} flex-row `}>
-
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6 `}>
+      {stats.map((stats)=>(
+        <div key={stats.id} className={`flex-1 flex justify-start items-center flex-row m-3 `}>
+          <h4 className="font-poppins font-semibold xs:leading-[53px] xs:text-[40px] text-[30px] leading-[43px] text-white  ">
+            {stats.value}
+          </h4>
+          <p className="font-poppins font-normal xs:leading-[26px] xs:text-[20px] text-[15px] leading-[21px] text-gradient uppercase ml-3  ">
+            {stats.title}            
+          </p>
+          
+        </div>
+      ))}
     </section>
   )
 
-export default Stats
+export default Stats;
