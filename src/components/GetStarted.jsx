7a6ba2dfda0c21c4e45bb3dc269434e44d27cf3d
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-04-04 18:57:18 +0100

    Akan: updated

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+const GetStarted = () => (
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer `}
+  >
+    <div className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}>
+      <div className={`${styles.flexStart} flex-row `}>
+        <p className="font-poppins">
+          <span>Get</span>
+        </p>
+      </div>
 
-export default GetStarted
+    </div>
+  </div>
+);
+
+export default GetStarted;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../style";
 import { discount, robot } from "../assets";
+import GetStarted from './GetStarted'
 
 const Hero = () => (
   <section
@@ -23,12 +24,11 @@ const Hero = () => (
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] ">
           The Next <br className="sm:block hidden"/> {' '}
-          <span className="text-gradient">Generation</span>  {' '} 
-          Payment Method.
+          <span className="text-gradient">Generation</span>  {' '} <br /> 
+            Payment Method.
         </h1>
         <div className="ss:flex hidden md:mr-4 mr-0 ">
-          
-
+          <GetStarted />
         </div>
       </div>
 
