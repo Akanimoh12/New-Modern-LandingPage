d9b520ba92867ff75bc3d262b92ed6bcb508c204
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-04-08 00:37:56 +0100

    Akan: Updated the Clients sections

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -15,11 +15,17 @@ const Testimonials = () => (
       </h1>
 
       <div className="w-full md:mt-0 mt-6">
-        <p className={`${styles.paragraph} text-left`}>
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
           Everything you need to accept card payments and grow your business
           anywhere on the planet.
         </p>
       </div>
+    </div>
+
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card)=>(
+        <FeedbackCard key={card.id} {...card}/>
+      ))}
     </div>
   </section>
 );
